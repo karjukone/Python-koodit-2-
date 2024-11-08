1c665a22c1a3c52da043380986208e1e35493b1c
Anni <anni.alanen@metropolia.fi>
2024-11-08 13:20:39 +0200

    jotai

--- 13 tehtävät/13.1.py
+++ 13 tehtävät/13.1.py
@@ -1,1 +1,24 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/alkuluku/<int:luku>')
+def onkoAlkuluku(luku):
+    onAlku = True
+
+    if luku < 2:
+        onAlku = False
+
+    for jakaja in range(2, luku):
+        if luku % jakaja == 0:
+            onAlku = False
+            break
+    
+    vastaus = {
+        "Number" : luku,
+        "isPrime" : "en tiiä"
+    }
+    return vastaus
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1')
